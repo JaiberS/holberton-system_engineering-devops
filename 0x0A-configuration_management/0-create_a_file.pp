b36974525a7cbebfe804d5f5ea15e9613cd51adc
JaiberS <758@holbertonschool.com>
2019-08-05 20:13:28 +0000

    [Feature] Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+# Using Puppet, create a file in /tmp
+file{"/tmp/holberton":
+  ensure  => "present",
+  owner   => "www-data",
+  group   => "www-data",
+  mode    => "0744",
+  content => "I love Puppet",
+}
