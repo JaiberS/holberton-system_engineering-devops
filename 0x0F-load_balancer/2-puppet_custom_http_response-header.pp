98c5b984655063c9d4c054e01522d49021d16efd
JaiberS <758@holbertonschool.com>
2019-08-22 23:31:19 +0000

    [Feature] html head

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,1 +1,4 @@
-#!/usr/bin/env bash
+# HTML header
+exec { 'change to head HTML':
+  command => '/usr/bin/sudo /bin/sed -i "14i\ \tadd_header X-Served-By $(cat /etc/hostname);" /etc/nginx/nginx.conf && /usr/bin/sudo /usr/sbin/service nginx restart',
+    }
