d1c592086e9a14ed43fe9531c5bbcaa4ba83893e
JaiberS <758@holbertonschool.com>
2019-08-22 23:38:16 +0000

    [Fixes] added the other part of the 0 task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,4 +1,4 @@
 # HTML header
 exec { 'change to head HTML':
-  command => '/usr/bin/sudo /bin/sed -i "14i\ \tadd_header X-Served-By $(cat /etc/hostname);" /etc/nginx/nginx.conf && /usr/bin/sudo /usr/sbin/service nginx restart',
+  command => '/usr/bin/sudo /usr/bin/apt-get -y update && /usr/bin/sudo /usr/bin/apt-get -y install nginx && /usr/bin/sudo /bin/echo "Holberton School" | /usr/bin/sudo /usr/bin/tee /var/www/html/index.html && /usr/bin/sudo /bin/sed -i "14i\ \tadd_header X-Served-By $(cat /etc/hostname);" /etc/nginx/nginx.conf && /usr/bin/sudo /usr/sbin/service nginx restart',
     }
