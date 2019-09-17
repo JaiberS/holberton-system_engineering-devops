6767be4c6d8a83c6bc6b38f340b1879328923528
JaiberS <758@holbertonschool.com>
2019-09-17 02:27:56 +0000

    [Feature] Get data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     """ Get information """
     str0 = "https://jsonplaceholder.typicode.com/"
     user_tasks = requests.get(str0 + "todos",
-                        params={"userId":argv[1]})
+                              params={"userId": argv[1]})
     user = requests.get(str0 + "users/" + argv[1])
     j = 0
     for i in user_tasks.json():
-        if i["completed"] == True:
+        if i["completed"]:
             j += 1
     str0 = "Employee " + user.json()["name"] + " is done with tasks("
     print(str0 + str(j) + "/" + str(len(user_tasks.json())) + "):")
     for i in user_tasks.json():
-        if i["completed"] == True:
+        if i["completed"]:
             print("\t " + i["title"])
