555afa0cffdaea2ab86ccba5303a9f2d8fe9141f
JaiberS <758@holbertonschool.com>
2019-09-17 03:12:18 +0000

    [Features] Get all APIs to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""  For a given employee ID, returns information """
+import requests
+import json
+
+if __name__ == "__main__":
+    """ Get information """
+    str0 = "https://jsonplaceholder.typicode.com/"
+    user_tasks = requests.get(str0 + "todos/")
+    user = requests.get(str0 + "users/")
+    to_json = {}
+    for j in user.json():
+        to_json1 = []
+        for i in user_tasks.json():
+            if j["id"] == i["userId"]:
+                to_json2 = {}
+                to_json2.setdefault("task", i["title"])
+                to_json2.setdefault("completed", i["completed"])
+                to_json2.setdefault("username", j["name"])
+                to_json1.append(to_json2)
+                to_json.setdefault(j["id"], to_json1)
+    with open("todo_all_employees.json", "w") as save:
+        json.dump(to_json, save)
