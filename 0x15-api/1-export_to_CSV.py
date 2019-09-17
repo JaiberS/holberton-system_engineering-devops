85f146d6a51d05e75d04550d6a678b46d90f68bd
JaiberS <758@holbertonschool.com>
2019-09-17 02:35:50 +0000

    [Feature] API exported to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""  For a given employee ID, returns information """
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    """ Get information """
+    str0 = "https://jsonplaceholder.typicode.com/"
+    user_tasks = requests.get(str0 + "todos",
+                              params={"userId": argv[1]})
+    user = requests.get(str0 + "users/" + argv[1])
+    with open(argv[1] + ".csv", "w") as csv0:
+        csv0_info = csv.writer(csv0, quoting=csv.QUOTE_ALL)
+        for i in user_tasks.json():
+            csv0_info.writerow([i["userId"], user.json()["username"],
+                                i["completed"], i["title"]])
