31ed52af6670dfdc65fad5ce7cd12d29a1394c67
JaiberS <758@holbertonschool.com>
2019-10-02 22:24:30 +0000

    [Feature] Change typo on file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,4 @@
-#!/usr/bin/env bash
+# change a line in a file
+exec { 'change a line':
+  command => '/usr/bin/sudo /bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+    }
